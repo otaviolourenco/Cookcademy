583f2813fcf201864999dd5baa2babfe900fb9c9
Otávio Lourenço <otavioluissv@gmail.com>
2024-11-09 22:58:46 +0000

    Add recipes and Models structs

---
+++ Cookcademy/Models/Recipe.swift
@@ -0,0 +1,77 @@
+//
+//  Recipe.swift
+//  Cookcademy
+//
+//  Created by Otavio Lourenço on 09/11/2024.
+//
+
+import Foundation
+
+struct Recipe {
+    var mainInformation: MainInformation
+    var ingredients: [Ingredient]
+    var directions: [Direction]
+    
+    init(mainInformation: MainInformation, ingredients: [Ingredient], directions: [Direction]) {
+        self.mainInformation = mainInformation
+        self.ingredients = ingredients
+        self.directions = directions
+    }
+    
+    init() {
+        self.init(mainInformation: MainInformation(name: "", description: "", author: "", category: .breakfast),
+                  ingredients: [],
+                  directions: [])
+    }
+}
+
+struct MainInformation {
+    var name: String
+    var description: String
+    var author: String
+    var category: Category
+    
+    enum Category: String, CaseIterable {
+        case breakfast = "Breakfast"
+        case lunch = "Lunch"
+        case dinner = "Dinner"
+        case dessert = "Dessert"
+    }
+}
+
+struct Ingredient {
+    var name: String
+    var quantity: Double
+    var unit: Unit
+    
+    var description: String {
+        let formattedQuantity = String(format: "%g", quantity)
+        switch unit {
+        case .none:
+                let formattedName = quantity == 1 ? name : "\(name)s"
+                return "\(formattedQuantity) \(formattedName)"
+        default:
+            if quantity == 1 {
+                return "1 \(unit.singularName) \(name)"
+            } else {
+                return "\(formattedQuantity) \(unit.rawValue) \(name)"
+            }
+        }
+    }
+    
+    enum Unit: String, CaseIterable {
+        case oz = "Ounces"
+        case g = "Grams"
+        case cups = "Cups"
+        case tbs = "Tablespoons"
+        case tsp = "Teaspoons"
+        case none = "No units"
+        
+        var singularName: String { String(rawValue.dropLast())} // This will drop the last character, turning the world in singular
+    }
+}
+
+struct Direction {
+    var description: String
+    var isOptional: Bool
+}
