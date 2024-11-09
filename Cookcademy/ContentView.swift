ade164d49ea0597c2aceaeb274d459d7c8324b00
Otávio Lourenço <otavioluissv@gmail.com>
2024-11-09 21:06:25 +0000

    Initiating project. It´s part of the Codecademy's iOS development course

---
+++ Cookcademy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cookcademy
+//
+//  Created by Otavio Lourenço on 09/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Otavio Lourenço on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
