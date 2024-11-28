8356b0aa52cd88e7363e7ea70f949486de9cef53
Otávio Lourenço <otavioluissv@gmail.com>
2024-11-28 20:35:35 +0000

    Added a new view to add recipe

---
+++ Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
@@ -0,0 +1,46 @@
+//
+//  ModifyMainInformationView.swift
+//  Cookcademy
+//
+//  Created by Otavio Lourenço on 28/11/2024.
+//
+
+import SwiftUI
+
+struct ModifyMainInformationView: View {
+    @Binding var mainInformation: MainInformation
+    
+    var body: some View {
+        Form{
+            TextField("Recipe Names", text: $mainInformation.name)
+            TextField("Author", text: $mainInformation.author)
+            Section(header: Text("Description")){
+                TextEditor(text: $mainInformation.description)
+            }
+            Picker(selection: $mainInformation.category, label:
+                HStack{
+                    Text("Category")
+                    Spacer()
+                    Text(mainInformation.category.rawValue)
+            }) {
+                ForEach(MainInformation.Category.allCases, id: \.self) { category in
+                    Text(category.rawValue)
+                }
+            }
+            .pickerStyle(MenuPickerStyle())
+        }
+    }
+}
+
+struct ModifyMainInformationView_Previews: PreviewProvider {
+    @State static var mainInformation = MainInformation(name: "Test Name",
+                                                        description: "Test Description",
+                                                        author: "Test Author",
+                                                        category: .breakfast)
+    @State static var emptyInformation = MainInformation(name: "", description: "", author: "", category: .breakfast)
+        
+    static var previews: some View {
+        ModifyMainInformationView(mainInformation: $mainInformation)
+        ModifyMainInformationView(mainInformation: $emptyInformation)
+    }
+}
